68bd73d25aebaff728f83e63927fac2d5c14ee5f
Ramson Lonayo <ramsonlonayo@gmail.com>
2026-05-25 09:16:23 +0300

    feat: release PesaPulse v1.0 with real-time analytics and Laravel API integration

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.5.50.86:8000/api';
+  static const String baseUrl = 'https://pesapulse-t9hk.onrender.com/api';
   static String token = '';
 
   static Future<Map<String, dynamic>> loginUser(
